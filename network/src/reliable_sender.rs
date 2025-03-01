58ad8a2c3f6038eb18e3c8fedc8d492e327420a7
michael <michaeljustarock@gmail.com>
2025-03-01 12:34:06 +0000

    nits

--- network/src/reliable_sender.rs
+++ network/src/reliable_sender.rs
@@ -28,8 +28,8 @@ use std::os::unix::io::{AsRawFd, FromRawFd};
 pub mod reliable_sender_tests;
 
 // Constants for buffer sizes
-const TCP_SEND_BUF_SIZE: u32 =  4 * 1048576;  // 2MB send buffer
-const TCP_RECV_BUF_SIZE: u32 =  1 * 1048576;  // 2MB receive buffer for ACKs
+const TCP_SEND_BUF_SIZE: u32 =  2000_000;  // 2MB send buffer
+const TCP_RECV_BUF_SIZE: u32 =  500_000;  // 2MB receive buffer for ACKs
 const CHANNEL_BUFFER_SIZE: usize = 1_000;   // Internal channel buffer size
 
 /// Convenient alias for cancel handlers returned to the caller task.
